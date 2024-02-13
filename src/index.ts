5ec46b1e58013fa08e3addc23ad5b59e7fd46e29
Atifsid <atifsiddiqui35@gmail.com>
2024-02-13 23:40:42 +0530

    create random-id

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors';
 import { BaseResponseDTO } from './utils/DTOs/BaseResponseDTO';
 import { Doc } from './utils/DTOs/Doc';
+import { v4 as uuidv4 } from 'uuid';
 
 type Bindings = {
   DB: D1Database
@@ -32,7 +33,7 @@ app.get("/doc", async (c) => {
 app.post("/doc", async (c) => {
   try {
     const doc: Doc = await c.req.json();
-    const { duration } = (await c.env.DB.prepare('INSERT INTO Documents (id, title, content) VALUES (?1, ?2, ?3)').bind(doc.id, doc.title, doc.content).run()).meta;
+    const { duration } = (await c.env.DB.prepare('INSERT INTO Documents (id, title, content) VALUES (?1, ?2, ?3)').bind(uuidv4(), doc.title, doc.content).run()).meta;
     let res: BaseResponseDTO<any> = {
       code: 200,
       message: `Document Saved in ${duration} s`
