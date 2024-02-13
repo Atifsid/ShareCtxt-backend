e295b93f008aa702d57b1f74657e01af76ca97ba
Atifsid <atifsiddiqui35@gmail.com>
2024-02-13 22:52:59 +0530

    create save doc api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors';
+import { BaseResponseDTO } from './utils/DTOs/BaseResponseDTO';
+import { Doc } from './utils/DTOs/Doc';
 
 type Bindings = {
   DB: D1Database
@@ -10,16 +12,35 @@ const app = new Hono<{ Bindings: Bindings }>();
 app.use('*', cors());
 
 app.get('/', (c) => {
-  return c.text('Wong Server Works!')
+  return c.text('Wong Server Works!');
 });
 
 app.get("/doc", async (c) => {
   try {
-    let { results } = await c.env.DB.prepare(`SELECT * FROM Documents`).all()
-    return c.json(results)
+    let { results } = await c.env.DB.prepare(`SELECT * FROM Documents`).all();
+    const res: BaseResponseDTO<Record<string, unknown>[]> = {
+      code: 200,
+      message: 'Successfully fetched Documents',
+      data: results
+    }
+    return c.json(res);
   } catch (e) {
-    return c.json({ err: e }, 500)
+    return c.json({ err: e }, 500);
   }
 });
 
-export default app
+app.post("/doc", async (c) => {
+  try {
+    const doc: Doc = await c.req.json();
+    const { duration } = (await c.env.DB.prepare('INSERT INTO Documents (id, title, content) VALUES (?1, ?2, ?3)').bind(doc.id, doc.title, doc.content).run()).meta;
+    let res: BaseResponseDTO<any> = {
+      code: 200,
+      message: `Document Saved in ${duration} s`
+    }
+    return c.json(res);
+  } catch (e) {
+    return c.json({ err: e }, 500);
+  }
+});
+
+export default app;
