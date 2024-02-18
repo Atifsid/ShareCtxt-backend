d7df1606bcef07ba0665520bbb8cdbeda29ccf47
Atifsid <atifsiddiqui35@gmail.com>
2024-02-18 14:16:11 +0530

    return id of newly created entry

--- src/index.ts
+++ src/index.ts
@@ -54,10 +54,11 @@ app.get("/doc/:id", async (c) => {
 app.post("/doc", async (c) => {
   try {
     const doc: Doc = await c.req.json();
-    const { duration } = (await c.env.DB.prepare('INSERT INTO Documents (id, title, content) VALUES (?1, ?2, ?3)').bind(uuidv4(), doc.title, doc.content).run()).meta;
+    const { duration, last_row_id } = (await c.env.DB.prepare('INSERT INTO Documents (id, title, content) VALUES (?1, ?2, ?3)').bind(uuidv4(), doc.title, doc.content).run()).meta;
     let res: BaseResponseDTO<any> = {
       code: 200,
-      message: `Document Saved in ${duration} s`
+      message: `Document Saved in ${duration} s`,
+      data: last_row_id
     }
     return c.json(res);
   } catch (e) {
